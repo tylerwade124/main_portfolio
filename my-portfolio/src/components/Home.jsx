3311dbb974c8c5d3b57d143cc1ab42e2e72758a0
tyler <tylerwade124@gmail.com>
2023-01-11 10:16:20 -0500

    svg signature animation

--- my-portfolio/src/components/Home.jsx
+++ my-portfolio/src/components/Home.jsx
@@ -1,13 +1,10 @@
-
+import Signature from "./Signature"
 
 export default function Home () {
     return (
        <div className='home'>
-            <div className='typewriter-animation'>
-                <h3 className = 'description'>Hi, I am</h3>
-                <h3 className = 'description2'>Matthew Wade,</h3>
-                <h3 className = 'description3'>Software Engineer</h3>
-            </div>
+            <h3>Hello, my name is </h3>
+            <Signature />
        </div>
     )
 }
