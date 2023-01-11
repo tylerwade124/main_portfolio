c2a16dcb154e44acf02851cb5a19ea2a232151ac
tyler <tylerwade124@gmail.com>
2023-01-11 11:37:16 -0500

    home page progress

--- my-portfolio/src/components/Home.jsx
+++ my-portfolio/src/components/Home.jsx
@@ -3,8 +3,16 @@ import Signature from "./Signature"
 export default function Home () {
     return (
        <div className='home'>
-            <h3>Hello, my name is </h3>
-            <Signature />
+            <h3 className='intro'>Hello, my name is</h3>
+            <div className='signature'>
+                <div className="white-box"></div>
+                <Signature />
+            </div>
+            <div className="sub-text">
+                    <p className="sub-text-1">Software Engineer</p>
+                    <p className="sub-text-2">Full-Stack Web Developer</p>
+            </div>
+            
        </div>
     )
 }
