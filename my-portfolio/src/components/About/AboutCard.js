7cb511ae04f0ec0f377bbe86bdccca24423c1724
tyler <tylerwade124@gmail.com>
2023-01-16 17:00:58 -0500

    deploy

--- my-portfolio/src/components/About/AboutCard.js
+++ my-portfolio/src/components/About/AboutCard.js
@@ -6,7 +6,7 @@ function AboutCard() {
       <Card.Body>
         <blockquote className="blockquote mb-0">
           <p style={{ textAlign: "justify" }}>
-            ENTER ABOUT ME HERE 
+          As a seasoned tradesman and experienced crew leader, I bring a unique blend of practical skills and technical expertise to my role as a software engineer. My dedication to continuous learning and problem-solving, combined with my ability to effectively collaborate with team members, allows me to deliver thoughtful solutions and valuable insights to any project. I am driven by my passion for using technology to create real-world impact.
           </p>
        
         </blockquote>
